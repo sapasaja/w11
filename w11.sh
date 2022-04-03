d7b4637b824307c80581f94b52b061065aed8b72
kulit_kuwaci <94762995+sapasaja@users.noreply.github.com>
2022-04-03 17:38:45 +0700

    Update w11.sh

--- w11.sh
+++ w11.sh
@@ -7,15 +7,15 @@ wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/nu
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 read -p ">> bismillah >>: " CRP 
 ./ngrok authtoken $CRP &>/dev/null &
-nohup ./ngrok tcp 30889 &>/dev/null &
+nohup ./ngrok tcp 3389 &>/dev/null &
 echo "############################################################################################"
 wget -O lite11.qcow2 --no-check-certificate https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.0.2/providers/qemu.box > /dev/null 2>&1
 echo "#..%%%%...%%..%%..%%..%%...%%%%.....%%..%%..%%..%%..%%..%%..%%..%%..%%..%%..%%%%%....%%%%..#"
-lscpu > /dev/null 2>&1
-echo "#.%%..%%..%%%.%%..%%%.%%..%%..%%.....%%%%...%%..%%..%%.%%...%%..%%..%%..%%..%%..%%..%%..%%.#"
 apt update > /dev/null 2>&1
-echo "#.%%..%%..%%.%%%..%%.%%%..%%..%%......%%....%%..%%..%%%%....%%..%%%%%%..%%..%%%%%...%%..%%.#"
+echo "#.%%..%%..%%%.%%..%%%.%%..%%..%%.....%%%%...%%..%%..%%.%%...%%..%%..%%..%%..%%..%%..%%..%%.#"
 apt install qemu -y > /dev/null 2>&1
+echo "#.%%..%%..%%.%%%..%%.%%%..%%..%%......%%....%%..%%..%%%%....%%..%%%%%%..%%..%%%%%...%%..%%.#"
+apt install qemu-kvm -y > /dev/null 2>&1
 echo "#.%%..%%..%%..%%..%%..%%..%%..%%......%%....%%..%%..%%.%%...%%..%%..%%..%%..%%..%%..%%..%%.#"
 apt install qemu-system -y > /dev/null 2>&1
 echo "#..%%%%...%%..%%..%%..%%...%%%%.......%%.....%%%%...%%..%%..%%..%%..%%..%%..%%..%%...%%%%..#"
