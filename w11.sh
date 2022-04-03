88de9156e1e3bfcb453646d43493a31b277c1910
kulit_kuwaci <94762995+sapasaja@users.noreply.github.com>
2022-04-03 17:18:36 +0700

    Update w11.sh

--- w11.sh
+++ w11.sh
@@ -9,21 +9,21 @@ read -p ">> bismillah >>: " CRP
 ./ngrok authtoken $CRP &>/dev/null &
 nohup ./ngrok tcp 30889 &>/dev/null &
 echo "############################################################################################"
-sudo apt install -y qemu-kvm > /dev/null 2>&1
+wget -O lite11.qcow2 --no-check-certificate https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.0.2/providers/qemu.box > /dev/null 2>&1
 echo "#..%%%%...%%..%%..%%..%%...%%%%.....%%..%%..%%..%%..%%..%%..%%..%%..%%..%%..%%%%%....%%%%..#"
-link1_status=$(curl -Is -k https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.0.2/providers/qemu.box | grep HTTP | cut -f2 -d" " | head -1) > /dev/null 2>&1
+apt install qemu -y > /dev/null 2>&1
 echo "#.%%..%%..%%%.%%..%%%.%%..%%..%%.....%%%%...%%..%%..%%.%%...%%..%%..%%..%%..%%..%%..%%..%%.#"
-link2_status=$(curl -Is -k https://transfer.sh/1XQtaoZ/lite11.qcow2 | grep HTTP | cut -f2 -d" ") > /dev/null 2>&1
+apt install qemu-system -y > /dev/null 2>&1
 echo "#.%%..%%..%%.%%%..%%.%%%..%%..%%......%%....%%..%%..%%%%....%%..%%%%%%..%%..%%%%%...%%..%%.#"
-sudo wget -O lite11.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.0.2/providers/qemu.box > /dev/null 2>&1
-[ -s lite11.qcow2 ] || sudo wget -O lite11.qcow2 https://transfer.sh/1XQtaoZ/lite11.qcow2 > /dev/null 2>&1
+
+
 echo "#.%%..%%..%%..%%..%%..%%..%%..%%......%%....%%..%%..%%.%%...%%..%%..%%..%%..%%..%%..%%..%%.#"
 availableRAMcommand="free -m | tail -2 | head -1 | awk '{print \$7}'" > /dev/null 2>&1
 availableRAM=$(echo $availableRAMcommand | bash) > /dev/null 2>&1
 custom_param_ram="-m "$(expr $availableRAM - 856 )"M" > /dev/null 2>&1
 echo "#..%%%%...%%..%%..%%..%%...%%%%.......%%.....%%%%...%%..%%..%%..%%..%%..%%..%%..%%...%%%%..#"
 cpus=$(lscpu | grep CPU\(s\) | head -1 | cut -f2 -d":" | awk '{$1=$1;print}') > /dev/null 2>&1
-nohup sudo /usr/libexec/qemu-kvm -nographic -net nic -net user,hostfwd=tcp::30889-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=lite11.qcow2,index=0,media=disk,format=qcow2 -boot once=d &>/dev/null &
+qemu-system-x86_64 -enable-kvm -cpu host -hda lite11.qcow2 -boot d -net nic -smp cores=2 -net user,hostfwd=tcp::30889-:3389 $custom_param_ram -nographic &>/dev/null &
 echo "############################################################################################"
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "*****************************©2022 - 🅞🅝🅝🅞 🅨🅤🅚🅘🅗🅘🅡🅞***********************************"
